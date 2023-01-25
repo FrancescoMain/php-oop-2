78b013beed8e39827974773622c21feac1c1f648
francesco <cesaranofrancescomain@gmail.com>
2023-01-25 16:45:50 +0100

    creata classe prodotto

---
+++ models/Product.php
@@ -0,0 +1,82 @@
+<?php 
+
+class Product {
+    private $id;
+    private $name;
+    private $description;
+    private $price;
+    private $category;
+    private $image;
+    
+    public function __construct($id, $name, $description, $price, $category, $image) {
+        $this->setID($id);
+        $this->setName($name);
+        $this->setDescription($description);
+        $this->setPrice($price);
+        $this->setCategory($category);
+        $this->setImage($image);
+
+    }
+    
+    public function getID() {
+        return $this->id;
+    }
+    
+    public function setID($id) {
+        $this->id = $id;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function setName($name) {
+        if (strlen($name) <= 20) {
+            $this->name = $name;
+        } else {
+            return;
+        }
+    }
+    
+    public function getDescription() {
+        return $this->description;
+    }
+    
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+    
+    public function getPrice() {
+        return $this->price;
+    }
+    
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+    
+    public function getCategory() {
+        return $this->category;
+    }
+    
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+    
+    public function getImage() {
+        return $this->image;
+    }
+    
+    public function setImage($image) {
+        $this->image = $image;
+    }
+
+    public function getData() {
+        return 
+        "ID: " . $this->getID() . "<br>"
+        ."Nome: " . $this->getName() . "<br>"
+        ."Descrizione: " . $this->getDescription() . "<br>"
+        ."Prezzo: " . $this->getPrice() . "<br>"
+        ."Categoria: " . $this->getCategory()->getName() . "<br>"
+        ."Tipologia: " . $this->getCategory()->getTypeName() . "<br>";
+    }
+}
