92db0717536b5709c49ce9007310a8dfbdfee9f8
francesco <cesaranofrancescomain@gmail.com>
2023-01-25 17:17:26 +0100

    eliminati commenti inutili e ristampato dati

--- index.php
+++ index.php
@@ -1,35 +1,27 @@
 <?php
     require_once 'models/Product.php';
     require_once 'models/Category.php';
-    require_once 'models/Type.php';
+    require_once 'models/Cibo.php';
+    require_once 'models/Gioco.php';
+    require_once 'models/Cuccia.php';
     require_once 'db.php';
 
 
-// $gamesCats = new Type(1, "GATTI", "Tutte le informazioni sui gatti", "Giochi");
-// $gamesDogs = new Type(2, "CANI", "Tutte le informazioni sui cani", "Giochi");
-// $foodCats = new Type(3, "GATTI", "Tutte le informazioni sui gatti", "Cibo");
-// $foodDogs = new Type(4, "CANI", "Tutte le informazioni sui cani", "Cibo");
-// $bedCats = new Type(5, "GATTI", "Tutte le informazioni sui gatti", "Cuccia");
-// $bedDogs = new Type(6, "CANI", "Tutte le informazioni sui cani", "Cuccia");
-
-// $palla = new Product(1, "Palla di gomma", "Una palla di gomma rimbalzante", 20.99, $gamesDogs, "palla.jpg",null) ;
-// $topo = new Product(2, "Topo Giocattolo", "Un topo finto", 10.99, $gamesCats, "topo.jpg",null) ;
-// $crocchetteCane = new Product(3, "Crocchette per cani", "Crocchette di media grandezza", 30.99, $foodDogs, "crocchette.jpg","24/12/2022") ;
-// $latteGatto = new Product(4, "Latte per gatti", "Latte per gatti appena nati", 4.99, $foodCats, "latte.jpg","24/01/2022") ;
-// $cucciaCane = new Product(5, "Cuccia per cane", "Cuccia per cane taglia grande", 50.99, $bedDogs, "cucciaXL.jpg", null) ;
-// $cucciaGatto = new Product(6, "Cuccia per gatti", "Cuccia per gatti appena nati", 13.99, $bedCats, "cucciaS.jpg", null) ;
 
 
-// echo $palla->getData();
-// echo "<br> <br>";
-// echo $topo->getData();
-// echo "<br> <br>";
-// echo $crocchetteCane->getData();
-// echo "<br> <br>";
-// echo $latteGatto->getData();
-// echo "<br> <br>";
-// echo $cucciaCane->getData();
-// echo "<br> <br>";
-// echo $cucciaGatto->getData();
+
+
+
+echo $palla->getData();
+echo "<br> <br>";
+echo $topo->getData();
+echo "<br> <br>";
+echo $crocchette->getData();
+echo "<br> <br>";
+echo $latte->getData();
+echo "<br> <br>";
+echo $cucciaGrande->getData();
+echo "<br> <br>";
+echo $cucciaGatto->getData();
 
 
